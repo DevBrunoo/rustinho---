7f95ded1a0ada925f268bcfe9f4f2b9ea2772c10
Bruno Oliveira <brunocosta1699320@gmail.com>
2023-06-01 18:26:44 -0300

    add creat

---
+++ day1/names.rs
@@ -0,0 +1,17 @@
+fn main() {
+    
+    let idade_lua = 19;
+    let idade_bruno = 20;
+
+    println!("A linda tem {} anos", idade_lua);
+    println!("O lindo tem {} anos", idade_bruno);
+
+
+}
+
+
+/* 
+FN e usado para declarar ma funcao no rust
+Let e usado para declarar uma variavel no rust 
+println!() e usado para imprimir uma mensagem na tela porem a mensagem precisa estar em aspas 
+ */
