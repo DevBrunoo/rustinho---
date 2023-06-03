8515e517f1f75dfbf2be737d21a7c03c0037817c
Bruno Oliveira <brunocosta1699320@gmail.com>
2023-06-03 01:10:04 -0300

    add cargo

---
+++ proj/target/debug/deps/proj-5f743fc09bbb4e90.d
@@ -0,0 +1,5 @@
+/Users/brunocosta/rustinho----1/proj/target/debug/deps/proj-5f743fc09bbb4e90: src/main.rs
+
+/Users/brunocosta/rustinho----1/proj/target/debug/deps/proj-5f743fc09bbb4e90.d: src/main.rs
+
+src/main.rs:
---
+++ proj/target/debug/proj.d
@@ -0,0 +1,1 @@
+/Users/brunocosta/rustinho----1/proj/target/debug/proj: /Users/brunocosta/rustinho----1/proj/src/main.rs
