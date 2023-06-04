16f5eb33352aa5d26ad31e53292fda42635a961b
Bruno Oliveira <brunocosta1699320@gmail.com>
2023-06-04 15:45:07 -0300

    add all

---
+++ day3/tst.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    let mut s = String::new();
+    println!("Digite um numerozinhooo");
+
+    io::stdin()
+        .read_line(%mut s)
+        .expect("Error na digitacao broo");
+
+    println!("Voce colocou {s}")
+}
